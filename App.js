3c9a6fe946da06aece8508fad6e5af6425474c2a
BayleyD <Bayley.D021001@gmail.com>
2022-09-24 20:52:50 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import reactDom from 'react-dom';
-import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert, TextInput } from 'react-native';
 
-const App = () => (
-  <View style = {styles.timeCont}>
-    <View style = {styles.box}>
-
-    </View>
-    <View style = {styles.box}>
-
-    </View>
-  </View>
-  
-);
+export default function App(){
+  const [time, setTime] = useState(0);
+  return(
+    <SafeAreaView style = {styles.container}>
+      <View style = {styles.box}>
+        <TextInput 
+          style = {styles.input}
+          placeholder = 'e.g. 600'
+          onSubmitEditing={(val) => setTime(val)}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
 
 const styles = StyleSheet.create({
-  timeCont: {
+  container: {
     height: '100%',
     width: '100%',
-    backgroundColor: 'red',
+    backgroundColor: 'black',
     flexDirection: 'row',
     flexWrap: 'wrap',
   },
@@ -26,8 +29,14 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '20%',
     backgroundColor: 'blue',
-    marginTop: '5dp',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    backgroundColor: 'white',
+    padding: 8,
+    margin: 10,
+    width: 200,
   }
 });
-
-export default App;
