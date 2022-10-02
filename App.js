2ffc0c61eafb7ca6a8f82d8b2cce5c775216a5cd
BayleyD <Bayley.D021001@gmail.com>
2022-10-02 08:49:35 -0500

    Added in some structuring and such

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React, { useState, Component } from 'react';
-import reactDom from 'react-dom';
 import { StyleSheet, Button, View, SafeAreaView, Text, Alert, TextInput } from 'react-native';
 
 
@@ -36,70 +35,5 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     alignContent: 'center',
     alignItems: 'center',
-  },
-  box: {
-    width: '100%',
-    backgroundColor: 'blue',
-    alignItems: 'center',
-    justifyContent: 'center',
-    display: 'flex',
-  },
-  input: {
-    borderWidth: 1,
-    backgroundColor: 'white',
-    padding: 8,
-    margin: 10,
-    width: 200,
-    textAlign: 'center',
   }
 });
-
-class TimerComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      seconds: parseInt(props.startTimeInSeconds, 10) || 60,
-    };
-  }
-
-  render() {
-    return (
-        <View> 
-            <Text style={this.props.style}>
-                {this.formatTime(this.state.seconds)}
-            </Text>
-        </View>
-    );
-  }
-
-  componentDidMount() {
-    this.interval = setInterval(() => this.decrementTime(), 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  decrementTime() {
-    if(this.state.seconds >= 0) {
-        this.setState(state => ({
-            seconds: state.seconds - 1
-    }))};
-    if(this.state.seconds == 0) {
-        alert("Timer Ended");
-    }
-  }
-
-  formatTime(timePassedInSeconds) {
-    if(timePassedInSeconds < 0) {
-        return "Timer Ended"
-    }
-    let hours = Math.floor(timePassedInSeconds / 3600);
-    let minutes = Math.floor(timePassedInSeconds / 60) % 60;
-    let seconds = timePassedInSeconds % 60;
-    let returnArray = [hours, minutes, seconds]
-        .map(time => String(time).padStart(2, '0'))
-        .join(':');
-    return returnArray
-  }
-}
