d7504131df8d6ea7f326a9f1f7ec0ebfc926c05d
LiquidEnder <79119607+LiquidEnder@users.noreply.github.com>
2022-11-13 18:25:51 -0600

    added some sign in functionality
    Also deleted my old branch and made a new one. It was easier than merging.

--- firebase.js
+++ firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 //import { initializeApp } from "firebase/app";
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, createUserWithEmailAndPassword,
+         signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,3 +19,41 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp( firebaseConfig );
 export const auth = getAuth( app );
+
+//create new user
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
+  //sign in existing users
+signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+
+//set up an observer and get user data, needs to be done for every page that might need user info
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
